22e782c247358e201d6c659ee890fc58233e5105
Yuki Ito <37338201+ykit00@users.noreply.github.com>
2024-12-09 11:42:13 +0900

    chore: Update devDependencies and adjust Vite base URL for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/hello-vite/',
+  base: process.env.GITHUB_ACTIONS ? '/hello-vite/' : '/',
   plugins: [react()],
 })
